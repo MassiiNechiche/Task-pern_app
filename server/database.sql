5b7812849ce69d2e4e94d0a838b8164d6f5a32ca
Massii <massinechiche@gmail.com>
2021-10-01 12:48:00 +0200

    create todo endpoint

--- server/database.sql
+++ server/database.sql
@@ -2,5 +2,5 @@ CREATE DATABASE task;
 
 CREATE TABLE todo(
     todo_id SERIAL PRIMARY KEY,
-    descripton VARCHAR(255)
+    description VARCHAR(255)
 );
--- server/server.js
+++ server/server.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const cors = require("cors");
 
+const pool = require("./db");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Create todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES ($1) RETURNING *",
+      [description]
+    );
+
+    res.json(newTodo);
+  } catch (err) {
+    console.log(err);
+  }
+});
+// get all todos
+// get a todo
+// update a todo
+// delete a todo
 
 app.listen(5000, () => {
   console.log("server running on port 5000");
