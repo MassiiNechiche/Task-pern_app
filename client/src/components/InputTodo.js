bee8040b52f79840df78af28bfe4c79bed1cd24d
Massii <massinechiche@gmail.com>
2021-10-02 09:24:18 +0200

    Input todo

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function InputTodo() {
+  const [description, setDescription] = useState("");
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:5000/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <h1 className="text-center mt-5">Task</h1>
-      <form className="d-flex mt-5">
-        <input type="text" className="form-control" />
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          className="form-control"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
         <button className="btn btn-success ">Add</button>
       </form>
     </>
